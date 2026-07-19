66f5defc72265a1c8af86fd43d8e174346cea685
ns-2563 <snagendrasingh25@gmail.com>
2026-07-19 10:47:11 +0530

    Refactor Resume feature into self-contained module

--- frontend/src/resume/pages/ResumeStudio.jsx
+++ frontend/src/resume/pages/ResumeStudio.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-import ResumeBuilder from "@/components/resume/ResumeBuilder";
+import ResumeBuilder from "@/resume/components/ResumeBuilder";
 import { useResumeContext } from "@/resume/context/useResumeContext";
 
 export default function ResumeStudio() {
